3b8c4961a939ab6914922eb91a6c37c1a2274a4c
empsgit <empdtj@gmail.com>
2017-02-03 21:22:28 +0500

    initial commit

---
+++ rofi/rofi.sh
@@ -0,0 +1,2 @@
+rofi -show run -lines 5 -eh 2  -separator-style none -fullscreen -location 0 -padding 220 -opacity "90" -hide-scrollbar -bw 0 -color-window "#1f222d, #ffffff" -color-normal "#1f222d,#ffffff,#1f222d,#1f222d,#9575cd" -font "Roboto 15"
+
